59f9177041256c9a3b7ce576b5e791db58c4f9ad
Kim Goetzke <120580433+kimgoetzke@users.noreply.github.com>
2023-01-18 05:18:54 +0000

    Update WebQuizEngine.java

--- Web Quiz Engine/task/src/engine/WebQuizEngine.java
+++ Web Quiz Engine/task/src/engine/WebQuizEngine.java
@@ -9,11 +9,4 @@ public class WebQuizEngine {
         SpringApplication.run(WebQuizEngine.class, args);
         System.out.println("We're live.");
     }
-
-    // Solutions to Stage 1: https://hyperskill.org/projects/91/stages/504/implement#solutions
-    // Solutions to Stage 2: https://hyperskill.org/projects/91/stages/505/implement#solutions
-    // Solutions to Stage 3: https://hyperskill.org/projects/91/stages/506/implement#solutions
-    // Solutions to Stage 4: https://hyperskill.org/projects/91/stages/507/implement#solutions
-    // Solutions to Stage 5: https://hyperskill.org/projects/91/stages/508/implement#solutions
-    // Solutions to Stage 6: https://hyperskill.org/projects/91/stages/509/implement#solutions
 }
